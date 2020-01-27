e402e8723370ecd6c9c2c572464c64caffb3cb1d
Chytra K R <34940260+chytrakr@users.noreply.github.com>
2020-01-27 16:53:58 +0530

    Update github-users.service.ts

--- src/app/github-users/github-users.service.ts
+++ src/app/github-users/github-users.service.ts
@@ -8,6 +8,6 @@ export class GithubUsersService {
   constructor(private http: HttpClient) { }
 
   fetchTableData(url): Promise<any> {
-    return this.http.get(url, { headers: { Authorization: 'token 31258820d06ef8c2a99ca60ecb14c8c51ffc5384', 'Content-Type': 'application/json'}}).toPromise();
+    return this.http.get(url, { headers: { Authorization: 'token <github_oauth_token', 'Content-Type': 'application/json'}}).toPromise();
   }
 }
